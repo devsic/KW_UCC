4bfbd3bce37e7fb5ea6e77381947b7971c551f17
AN HONG KYUNG <ahk959595@gmail.com>
2019-02-24 23:55:56 +0900

    fcm REST,db 수정

--- Node/app.js
+++ Node/app.js
@@ -3,7 +3,11 @@ var app = express();
 var db = require('./db')
 
 var UserController = require('./models/UserController');
+var InfoController = require('./models/InfoController');
+var Fcm = require('./models/fcm');
 // router 등록
 app.use(UserController);
+app.use(InfoController);
+app.use(Fcm);
 
 module.exports = app;
--- Node/models/info.js
+++ Node/models/info.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 // Schema는 document(tuple)의 구조를 보여줌.
 // document가 모여서 group(table)
-var userSchema = new Schema({
+var infoSchema = new Schema({
     user_id: String,
-    beacon_id: String
+    gps: String,
+    timestamp : Date
 });
 
-// 위에서 UserSchema를 만들어놓고 require 하는 것.
 // module.exports를 사용하여 해당 model을 반환.
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model('info', infoSchema);
